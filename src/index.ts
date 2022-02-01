f8cc45bd6c4df8e99d0fdf9373922fd879e30f6c
Tommi Lepola <lepola.tommi@gmail.com>
2022-02-01 18:13:56 +0200

    Add keypress event to stop bot

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { Client } from "discord.js";
 import ready from './listeners/ready';
 import interactionCreate from './listeners/interaction_create';
 import 'dotenv/config';
+import readline from 'readline';
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
 
 console.log("Bot is starting...");
 const token = process.env['TERRARIA_BOT_TOKEN'];
@@ -13,3 +16,10 @@ ready(client);
 interactionCreate(client);
 
 client.login(token);
+
+process.stdin.on('keypress', (str, key) => {
+  if (key.name === 'q') {
+    client.destroy();
+    process.exit();
+  }
+});
