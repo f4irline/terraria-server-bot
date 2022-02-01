4bfeaf94b406c3e03a2efbc764d96324b6a6df20
Tommi Lepola <lepola.tommi@gmail.com>
2022-02-01 19:02:17 +0200

    Add server commands

---
+++ src/commands/server.ts
@@ -0,0 +1,68 @@
+import { BaseCommandInteraction, Client } from "discord.js";
+import { Command } from "../models/command";
+import spawn from 'child_process';
+
+let serverProcess: spawn.ChildProcess | undefined;
+
+export const startServer: Command = {
+  name: "start",
+  description: "Starts the terraria server",
+  type: "CHAT_INPUT",
+  run: async (_client: Client, interaction: BaseCommandInteraction) => {
+    const PATH_TO_START_SCRIPT = process.env['PATH_TO_START_SCRIPT'];
+    if (!PATH_TO_START_SCRIPT) {
+      await interaction.followUp({
+        content: 'Could not start server. PATH_TO_START_SCRIPT environment variable is not set.'
+      });
+      throw new Error('PATH_TO_START_SCRIPT environment variable not set!');
+    }
+
+    if (serverProcess) {
+      serverProcess.kill();
+      serverProcess = undefined;
+    }
+
+    serverProcess = spawn.exec(`cmd /c "${PATH_TO_START_SCRIPT}"`);
+
+    serverProcess.addListener('spawn', async () => {
+      await interaction.followUp({
+        ephemeral: true,
+        content: 'Server started successfully!',
+      });
+    });
+  }
+};
+
+export const stopServer: Command = {
+  name: "stop",
+  description: "Stops the terraria server",
+  type: "CHAT_INPUT",
+  run: async (_client: Client, interaction: BaseCommandInteraction) => {
+    if (serverProcess) {
+      serverProcess.stdin?.write('exit');
+      serverProcess.stdin?.end();
+      serverProcess = undefined;
+      await interaction.followUp({
+        ephemeral: true,
+        content: 'Server stopped successfully!',
+      });
+    }
+  }
+}
+
+export const status: Command = {
+  name: "status",
+  description: "Returns the status of the server",
+  type: "CHAT_INPUT",
+  run: async (_client: Client, interaction: BaseCommandInteraction) => {
+    serverProcess
+      ? await interaction.followUp({
+        ephemeral: true,
+        content: 'Server is running!',
+      })
+      : await interaction.followUp({
+        ephemeral: true,
+        content: 'Server is not running.',
+      });
+  }
+}
--- src/models/command.ts
+++ src/models/command.ts
@@ -1,8 +1,9 @@
 import { BaseCommandInteraction, ChatInputApplicationCommandData, Client } from "discord.js";
+import { startServer, stopServer, status } from "../commands/server";
 import { ping } from "../commands/ping";
 
 export interface Command extends ChatInputApplicationCommandData {
   run: (client: Client, interaction: BaseCommandInteraction) => void;
 }
 
-export const commands: Command[] = [ping];
+export const commands: Command[] = [ping, startServer, stopServer, status];
