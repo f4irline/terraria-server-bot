6c5d3b6a7f0eb6c24274fb3327e342581454beaa
Tommi Lepola <lepola.tommi@gmail.com>
2022-02-05 14:30:48 +0200

    Add some dev-run config

--- discord-bot/webpack.dev.js
+++ discord-bot/webpack.dev.js
@@ -1,7 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 module.exports = merge(common, {
   mode: 'development',
   watch: true,
+  plugins: [
+    new WebpackShellPluginNext({
+      onBuildEnd: {
+        scripts: ['yarn run:nodemon'],
+        blocking: false,
+        parallel: true
+      }
+    })
+  ],
 });
